dcf4ccb7d6e6c69f577bd625e4584f644d709b39
Naeem Islam <85617842+naeemexe@users.noreply.github.com>
2025-12-30 01:44:02 -0600

    Updated formatting

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-let myTasks = JSON.parse(localStorage.getItem('savedtask'));
-if(myTasks===null){
-    myTasks=[];
-}
+const myTasks = JSON.parse(localStorage.getItem('savedtask'));
+if(myTasks === null){
+    myTasks = []; }
 
-function addTask(){
-    let taskInput = document.querySelector('.js-input-task');
-    let dateInput = document.querySelector('.js-date-input');
+function addTask() {
+    const taskInput = document.querySelector('.js-input-task');
+    const dateInput = document.querySelector('.js-date-input');
     myTasks.push({name : taskInput.value,
                   date : dateInput.value});
-    taskInput.value='';
+    taskInput.value = '';
     localStorage.setItem('savedtask',JSON.stringify(myTasks));
 }
-
-function showTask(){
-    let html ='';
-    //console.log(myTasks);
-    for(index=0;index<myTasks.length;index++){
+function showTask() {
+    const html = '';
+    for(index = 0; index < myTasks.length; index++) {
         html = html + `<div>${myTasks[index].name}</div>
                        <div>${myTasks[index].date}</div>   
-                       <button onclick ="deleteTask(${index});" class="delete-button">Delete</button>`;}
-
+                       <button onclick = "deleteTask(${index});" class = "delete-button"> Delete </button>`; }
     document.querySelector('.js-output-task').innerHTML = html;
 }
 
-function deleteTask(index){
-    myTasks.splice(index,1);
+function deleteTask(index) {
+    myTasks.splice(index, 1);
     localStorage.setItem('savedtask',JSON.stringify(myTasks));
     showTask(); 
 }
-
 showTask();
